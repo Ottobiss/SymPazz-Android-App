55da012109881a856cf801d37a1a90fc6ea22143
Otto von Bismarck <IudinSergey2007@gmail.com>
2024-02-24 22:25:28 +0300

    Switching to RegisterActivity from LoginActivity

--- app/src/main/java/com/cof/sympuzz/LoginActivity.java
+++ app/src/main/java/com/cof/sympuzz/LoginActivity.java
@@ -14,6 +14,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -42,6 +43,15 @@ public class LoginActivity extends AppCompatActivity {
         Password = findViewById(R.id.password);
 
         authProfile = FirebaseAuth.getInstance();
+
+        TextView CreateAcc = findViewById(R.id.createacc);
+
+        CreateAcc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+            }
+        });
 
         Button ButtonLogin = findViewById(R.id.logbtn);
         ButtonLogin.setOnClickListener(new View.OnClickListener() {
